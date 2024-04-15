8bfc96b4af5a8dbcbd8a77618f9f5a29167d9c98
iZer0ky <yokero90@gmail.com>
2024-04-15 01:33:31 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Menu from './components/Menu'
 import Portfolio from './components/Portfolio'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
